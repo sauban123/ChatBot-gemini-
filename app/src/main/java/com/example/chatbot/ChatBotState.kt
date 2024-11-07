90f2dccdef4fad48697a74420ad9a90ed17d1e83
Mohd Sauban <mosauban88@gmail.com>
2024-11-07 20:12:27 +0530

    Delete app directory

--- app/src/main/java/com/example/chatbot/ChatBotState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chatbot
-
-sealed interface ChatBotUiState{
-    data object  Ideal : ChatBotUiState
-    data object  Loading:ChatBotUiState
-    data class Success(val ChatData: String) :ChatBotUiState
-    data class Eroor(val ChatError : String) : ChatBotUiState
-
-}
