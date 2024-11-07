82a9bc2b468516a94a0fd556c50b583c4adbba1b
mohd sauban <mosauban88@gmail.com>
2024-11-07 20:26:17 +0530

    finished

---
+++ app/src/main/java/com/example/chatbot/ChatBotState.kt
@@ -0,0 +1,9 @@
+package com.example.chatbot
+
+sealed interface ChatBotUiState{
+    data object  Ideal : ChatBotUiState
+    data object  Loading:ChatBotUiState
+    data class Success(val ChatData: String) :ChatBotUiState
+    data class Eroor(val ChatError : String) : ChatBotUiState
+
+}
